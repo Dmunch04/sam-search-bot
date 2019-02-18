53e1731e3a35647358671d32223a9e81b0254f76
Daniel Munch <daniellmunch@gmail.com>
2019-02-18 17:15:19 +0100

    Updated Embed Helper Embed design

--- Helpers/EmbedHelper.py
+++ Helpers/EmbedHelper.py
@@ -1,5 +1,5 @@
 import discord
 
 async def ResultEmbed (client, title, content, link, channel):
-    embed = discord.Embed(title = title, url = link, description = content)
+    embed = discord.Embed(title = title, url = link, description = content, color = discord.Color.green())
     await client.send_message(channel, embed)
