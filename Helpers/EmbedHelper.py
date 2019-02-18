5d942fcefcc63fab030b02bb4726fd10ce6d6087
Daniel Munch <daniellmunch@gmail.com>
2019-02-18 17:17:52 +0100

    Forgot to actually identify the embed

--- Helpers/EmbedHelper.py
+++ Helpers/EmbedHelper.py
@@ -2,4 +2,4 @@ import discord
 
 async def ResultEmbed (client, title, content, link, channel):
     embed = discord.Embed(title = title, url = link, description = content, color = discord.Color.green())
-    await client.send_message(channel, embed)
+    await client.send_message(channel, embed = embed)
