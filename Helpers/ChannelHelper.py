bcdc4e54e508101ca8478c6b8e0d3a6e764375a2
Daniel Munch <daniellmunch@gmail.com>
2019-02-18 20:52:21 +0100

    Started a bit on ServerHelper and ChannelHelper

--- Helpers/ChannelHelper.py
+++ Helpers/ChannelHelper.py
@@ -1,3 +1,9 @@
 import discord
+from Helper import ServerHelper as serverHelper
+
+# Function for getting a channel by it's name
+async def get_channel (channel_name):
+    server = serverHelper.get_server()
+    print("Get channel")
 
 #https://discordpy.readthedocs.io/en/latest/api.html#discord.Channel
--- Helpers/ServerHelper.py
+++ Helpers/ServerHelper.py
@@ -1,3 +1,7 @@
 import discord
 
+# Function for getting the server
+async def get_server ():
+    print("Get the server")
+
 #https://discordpy.readthedocs.io/en/latest/api.html#discord.Server
