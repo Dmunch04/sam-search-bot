ff9e0f47ebfe6ff6daec2925fef826902fcff091
Daniel Munch <daniellmunch@gmail.com>
2019-02-21 17:56:44 +0100

    This is probably not gonna work either

--- Commands/Staff/UpdateCommand.py
+++ Commands/Staff/UpdateCommand.py
@@ -6,18 +6,6 @@ class CMD_Update:
     def __init__ (self, client):
         self.client = client
 
-    @commands.command(pass_context = True)
-    async def update (self, ctx, version):
-        author = ctx.message.author
-        channel = ctx.message.channel
-
-        if not 'Bot Dev' in [y.name.lower() for y in author.roles]:
-            await embed.CustomErrorEmbed(self.client, 'User Error', 'Permission not found', "Looks like you don't have the permissions to do that, buddy.", channel)
-            return
-
-        await updateBot(version)
-
-
     async def checkVersion ():
         newVersionFile = open('Data/newVersion.txt', 'r')
         newVersion = newVersionFile.read()
@@ -31,6 +19,17 @@ class CMD_Update:
             return
         else:
             await update.callback(newVersion)
+
+    @commands.command(pass_context = True)
+    async def update (self, ctx, version):
+        author = ctx.message.author
+        channel = ctx.message.channel
+
+        if not 'Bot Dev' in [y.name.lower() for y in author.roles]:
+            await embed.CustomErrorEmbed(self.client, 'User Error', 'Permission not found', "Looks like you don't have the permissions to do that, buddy.", channel)
+            return
+
+        await updateBot(version)
 
     async def updateBot (version):
         changelogFile = open('Data/changelog.txt', 'r')
