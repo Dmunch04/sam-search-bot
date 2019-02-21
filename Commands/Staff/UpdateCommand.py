d0af7933ec27c7c68af28b65c6e8b0ba105810f7
Daniel Munch <daniellmunch@gmail.com>
2019-02-21 20:00:22 +0100

    Didn't work

--- Commands/Staff/UpdateCommand.py
+++ Commands/Staff/UpdateCommand.py
@@ -6,9 +6,7 @@ from Helpers import ServerHelper as shelp
 class CMD_Update:
     def __init__ (self, client):
         self.client = client
-        await runMe()
-        self.runMeAswell()
-        await self.checkVersion()
+        #await self.checkVersion()
 
     @commands.command(pass_context = True)
     async def update (self, ctx):
@@ -53,12 +51,6 @@ class CMD_Update:
         curVersionFile = open('Data/currentVersion.txt', 'w')
         curVersionFile.write(newVersion)
         curVersionFile.close()
-
-        def runMeAswell (self):
-            print('Worksss!')
-
-async def runMe ():
-    print('Works')
 
 def setup (client):
     client.add_cog(CMD_Update(client))
